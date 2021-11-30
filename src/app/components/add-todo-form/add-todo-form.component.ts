a54b6b44c1b8d8f41659852f2fb5325223d05a20
softinside <p.taviani@reply.it>
2021-11-30 15:25:46 +0100

    Add desc and priority values

--- src/app/components/add-todo-form/add-todo-form.component.ts
+++ src/app/components/add-todo-form/add-todo-form.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-todo-form',
   templateUrl: './add-todo-form.component.html',
-  styleUrls: ['./add-todo-form.component.css']
+  styleUrls: ['./add-todo-form.component.css'],
 })
 export class AddTodoFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  //Creiamo il metodo onAddTask
+  onAddTask(desc: any, priority: any) {
+    console.log(desc.value, 'Desc');
+    console.log(priority.value, 'Desc');
   }
-
 }
